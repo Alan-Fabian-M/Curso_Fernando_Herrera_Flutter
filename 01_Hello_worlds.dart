9881cdab31aeddbadadd870dbf6b5ee979665801
Alan <alan.melgar.f@gmail.com>
2023-09-12 15:04:06 -0400

    Hola mundo - Dart

---
+++ 01_Hello_worlds.dart
@@ -0,0 +1,13 @@
+void main(){
+  
+//  var myName = 'Alan';
+  const myName = 'Alan';
+//  String myName = 'Alan';
+  
+ // myName = 'Mundo';
+  
+  print('Hola $myName');
+  print('Hola ${myname.toUpperCase() } ' );
+  print('Hola${1+1}');
+    
+}
