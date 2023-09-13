151b6adee65eba6e98090829571fa633848c128a
Alan <alan.melgar.f@gmail.com>
2023-09-13 00:19:55 -0400

    Parámetros con nombre

--- 05_functions.dart
+++ 05_functions.dart
@@ -4,6 +4,8 @@ void main(){
   
   print('Suma: ${addTwoNumbers(10, 20) }' );
   
+  print( greetPerson(name: 'Fernando', message: 'Hi,') );
+  
 }
 
 
@@ -12,9 +14,15 @@ String greetEveryone()=> 'Hello everyone!';
 int addTwoNumbers(int a, int b) => a + b;
 
 int addTwoNumbersOptional(int a, [ int b = 0 ]) {
-  
   // b ?? = 0;
-  
   return a + b;
 } 
+
+
+String greetPerson({required String? name, String message = 'Hola' }) {
+  
+  return '$message Alan';
+}
+
+
 
