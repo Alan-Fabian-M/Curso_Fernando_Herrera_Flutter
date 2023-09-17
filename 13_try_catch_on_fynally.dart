58ced67d847ed9b12df42491ac4187bff822bab9
Alan <alan.melgar.f@gmail.com>
2023-09-17 19:20:18 -0400

    Try, on, catch y finally

---
+++ 13_try_catch_on_fynally.dart
@@ -0,0 +1,31 @@
+void main() async{
+  
+  print('Inicio del programa');
+  
+  
+  try{
+    final value = await httpGet('http');
+    print( 'Exito : $value' );
+  } on Exception catch (err) {
+    print('Tenemos una exception: $err');
+  } catch(err){
+    print('OOP!! algo terrible paso: $err');
+    
+  } finally {
+    print('fin del try y catch');
+  }
+  
+  print('Fin del programa');
+  
+}
+
+Future<String> httpGet(String url) async {
+  
+  await Future.delayed( const Duration(seconds: 1));
+  
+  throw Exception('No hay parametros en el URL');
+  
+//   throw 'Error en la peticion';
+  
+//   return 'Tenemos un valor de la peticion';
+}
